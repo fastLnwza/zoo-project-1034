a3f2afeac8f37268e6ee6c12fb0642614946236b
fastLnwza <125943980+fastLnwza@users.noreply.github.com>
2024-11-16 17:10:30 +0000

    Addtest

--- test_zoo.py
+++ test_zoo.py
@@ -7,8 +7,14 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
+     # Add your additional test cases here.
+        self.assertEqual(self.zoo.get_ticket_price(-1),0)
+        self.assertEqual(self.zoo.get_ticket_price(0),50)
+        self.assertEqual(self.zoo.get_ticket_price(20),100)
+        self.assertEqual(self.zoo.get_ticket_price(21),150)
+        self.assertEqual(self.zoo.get_ticket_price(60),150)
        
-    # Add your additional test cases here.
+   
 
 if __name__ == '__main__':
     unittest.main()
